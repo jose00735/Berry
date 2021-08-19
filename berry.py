f3a1f446df4c2b97fc3fb286cd5acb756dafd741
EC2 Default User <ec2-user@ip-172-31-33-119.us-east-2.compute.internal>
2021-08-19 00:11:34 +0000

    bootstrap

--- berry.py
+++ berry.py
@@ -1,13 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return 'Hola mundo'
+    return render_template('home.html')
 
+@app.route("/about")
+def about():
+    return render_template('about.html', title='about')
 
 if __name__ == '__main__':
     app.run()
